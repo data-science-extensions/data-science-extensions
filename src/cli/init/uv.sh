316776a9586cef308aaa85a77c6601c629c30e19
chrimaho <44449504+chrimaho@users.noreply.github.com>
2025-03-29 13:16:17 +1100

    Add docs config

--- src/cli/init/uv.sh
+++ src/cli/init/uv.sh
@@ -1,7 +1,7 @@
 curl -LsSf https://astral.sh/uv/install.sh | sh
 uv self update
 uv init --python 3.13 --no-cache --name "data-science-extensions" --description "Data Science Extensions" --package
-uv add --no-cache typeguard
-uv add --no-cache --group "dev" black blacken-docs pre-commit isort codespell pyupgrade pylint pycln ipykernel
-uv add --no-cache --group "docs" mkdocs mkdocs-material mkdocstrings mkdocstrings-python mkdocs-coverage mkdocs-autorefs livereload mike black docstring-inheritance
-uv sync --no-cache --all-groups
+uv addn --link-mode=copy --no-cache typeguard
+uv addn --link-mode=copy --no-cache --group "dev" black blacken-docs pre-commit isort codespell pyupgrade pylint pycln ipykernel
+uv addn --link-mode=copy --no-cache --group "docs" mkdocs mkdocs-material mkdocstrings mkdocstrings-python mkdocs-coverage mkdocs-autorefs livereload mike black docstring-inheritance
+uv syncn --link-mode=copy --no-cache --all-groups
