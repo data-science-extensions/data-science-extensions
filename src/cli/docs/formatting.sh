4eb508850b03ae5e018de2c087e0aa62172ebf41
chrimaho <44449504+chrimaho@users.noreply.github.com>
2026-08-15 09:02:55 +1000

    Update formatting script to include header level for notebook conversion

--- src/cli/docs/formatting.sh
+++ src/cli/docs/formatting.sh
@@ -3,8 +3,8 @@ uv run extract-sections-from-markdown-file docs/guides/querying-data/index.md pa
 uv run extract-sections-from-markdown-file docs/guides/querying-data/index.md sql
 uv run extract-sections-from-markdown-file docs/guides/querying-data/index.md pyspark
 uv run extract-sections-from-markdown-file docs/guides/querying-data/index.md polars
-uv run reformat-and-convert-md-to-ipynb docs/guides/querying-data/index.md
-uv run reformat-and-convert-md-to-ipynb docs/guides/querying-data/index-pandas.md
-uv run reformat-and-convert-md-to-ipynb docs/guides/querying-data/index-sql.md
-uv run reformat-and-convert-md-to-ipynb docs/guides/querying-data/index-pyspark.md
-uv run reformat-and-convert-md-to-ipynb docs/guides/querying-data/index-polars.md
+uv run reformat-and-convert-md-to-ipynb docs/guides/querying-data/index.md h3
+uv run reformat-and-convert-md-to-ipynb docs/guides/querying-data/index-pandas.md h3
+uv run reformat-and-convert-md-to-ipynb docs/guides/querying-data/index-sql.md h3
+uv run reformat-and-convert-md-to-ipynb docs/guides/querying-data/index-pyspark.md h3
+uv run reformat-and-convert-md-to-ipynb docs/guides/querying-data/index-polars.md h3
